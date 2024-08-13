52727c0605ae5d078573ba235c050a9020d73eaf
Muhammad Ahmad <ahmad321ali007@gmail.com>
2024-08-13 14:09:26 +0500

    Add files via upload

---
+++ type.js
@@ -0,0 +1,5 @@
+console.log("This file is just for practice");
+// Functions in Typescript
+function add(x, y) {
+    return 5 + 4;
+}
---
+++ type.ts
@@ -0,0 +1,9 @@
+console.log("This file is just for practice");
+
+// Functions in Typescript
+
+function add(x: number, y: number): number{
+    return x + y ;
+}
+console.log(add(5 + 4 ));
+
